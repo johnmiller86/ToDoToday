a91ca42f17f67af96e3799378d07cae7b69bc489
johnmiller86 <chefjohn2006@hotmail.com>
2016-01-31 20:13:09 -0500

    Initial commit

--- app/src/main/java/com/ist402/jdm5908/todotoday/DBHelper.java
+++ app/src/main/java/com/ist402/jdm5908/todotoday/DBHelper.java
@@ -1,7 +1,4 @@
 package com.ist402.jdm5908.todotoday;
 
-/**
- * Created by chefj on 1/31/2016.
- */
 public class DBHelper {
 }
--- app/src/main/java/com/ist402/jdm5908/todotoday/ToDo_Item.java
+++ app/src/main/java/com/ist402/jdm5908/todotoday/ToDo_Item.java
@@ -1,7 +1,45 @@
 package com.ist402.jdm5908.todotoday;
 
-/**
- * Created by chefj on 1/31/2016.
- */
 public class ToDo_Item {
+
+    // Member Attributes
+    private int _id;
+    private String description;
+    private int is_done;
+
+    public ToDo_Item() {
+    }
+
+    public ToDo_Item(int id, String desc, int done) {
+
+        _id = id;
+        description = desc;
+        is_done = done;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getIs_done() {
+        return is_done;
+    }
+
+    public void setIs_done(int is_done) {
+        this.is_done = is_done;
+    }
+
+
 }
